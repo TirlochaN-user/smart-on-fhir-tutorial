e3d3bbbcadb6035f4ab16482243928c5b92f43a0
tirlochan <sunny.trilochan@gmail.com>
2020-12-14 13:28:29 +0530

    added code to fetch heart rate

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -20,7 +20,7 @@
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
-                              'http://loinc.org|29463-7']
+                              'http://loinc.org|29463-7','http://loinc.org|2708-6']
                       }
                     }
                   });
@@ -47,7 +47,9 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+
           var weight=byCodes('29463-7');
+          var heartrate=byCodes('2708-6');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -56,6 +58,7 @@
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
           p.weight = getQuantityValueAndUnit(weight[0]);
+          p.heartrate = getQuantityValueAndUnit(heartrate[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -89,7 +92,10 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
+
       weight: {value: ''},
+      heartrate:{value:''},
+
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -133,11 +139,13 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
+
     $('#weight').html(p.weight);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
+    $('#heartrate').html(p.heartrate);
+    // $('#systolicbp').html(p.systolicbp);
+    // $('#diastolicbp').html(p.diastolicbp);
+    // $('#ldl').html(p.ldl);
+    // $('#hdl').html(p.hdl);
   };
 
 })(window);
